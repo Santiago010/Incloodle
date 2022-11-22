208687ccfebdd31c6cab1b6cc21cd4e3de735ceb
Esteban G <tapitarias@gmail.com>
2022-11-21 21:01:18 -0800

    add fixes to present thrid phase

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://incloodle.herokuapp.com/v1",
-  baseURL: "http://127.0.0.1:64578/v1",
+  baseURL: "https://incloodle.herokuapp.com/v1",
   headers: {
     "Content-Type": "application/json",
   },
